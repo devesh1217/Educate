173105e07c664f0f0a174895c520fd25e3b34135
Devesh Mehta <devesh1217@gmail.com>
2024-01-04 22:32:20 +0530

    Ready for vercel

--- index.js
+++ index.js
@@ -27,86 +27,8 @@ server.use('/data', dataRouter);
 
 
 
-// Function to list files in a specific folder
-
-
-
-
-server.get('/api', async (req, res) => {
-    const folderId = '1ZoSD9YViVQQNWIGZD0FPXK-C5ywgnlQM';
-
-    const files = await listFilesInFolder(folderId);
-
-    if (files.length) {
-        res.json(files);
-    } else {
-        res.json({ message: 'No files found in the folder.' });
-    }
-});
-
-server.get('/data/:id', async (req, res) => {
-    const folderId = req.params.id;
-
-    try {
-        const files = await listFilesInFolder(folderId);
-        res.json(files);
-    } catch (err) {
-        console.log(err)
-        res.sendStatus(500);
-    }
-});
-
-server.post('/test', async (req, res) => {
-    const ansKey = await getGoogleSheetValues('1sh0LFSBxBRZS7IPLSwOK5FRlrddXXH8_iz51kxf38UM');
-    const ans = req.body.ans;
-    const correct = [];
-    const notMarked = [];
-    const inCorrect = [];
-    const to = 'u22cs057@coed.svnit.ac.in';
-    const sub = 'Test Result'
-
-
-    ans.forEach((curr, indx) => {
-        if (curr == '') {
-            notMarked.push(indx);
-        } else if (curr === ansKey[indx]) {
-            correct.push(indx);
-        } else {
-            inCorrect.push(indx);
-        }
-    });
-    const body = 'Correct: ' + correct.length + '<br>Incorrect: ' + inCorrect.length + '<br>Not marked: ' + notMarked.length;
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'u22cs035@coed.svnit.ac.in', // Replace with your Gmail email
-            pass: 'pkjncigmlpcxlurv',       // Replace with your Gmail password or use an server password
-        },
-    });
-
-    const mailOptions = {
-        from: 'u22cs035@coed.svnit.ac.in',
-        to: to,
-        subject: sub,
-        text: body,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-        } else {
-            console.log('Email sent:', info.response);
-        }
-    });
-
-    res.json({
-        correct,
-        inCorrect,
-        notMarked
-    });
-});
-
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default server;
